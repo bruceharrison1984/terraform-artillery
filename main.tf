57d8f5bf02728e37c821dbedf762b836e016717b
Bruce Harrison <leeharrison1984@gmail.com>
2017-05-08 21:47:57 -0500

    Remove shared s3 bucket

--- main.tf
+++ main.tf
@@ -1,29 +1,10 @@
 provider "aws" {
-  # alias  = "home_region"
   region = "us-east-1"
 }
 
-# resource "aws_s3_bucket" "artillery_packages" {
-#   bucket   = "artillery-packages"
-#   acl      = "private"
-#   provider = "aws.home_region"
-# }
-
-# resource "aws_s3_bucket_object" "artillery_package" {
-#   bucket   = "${aws_s3_bucket.artillery_packages.id}"
-#   key      = "package.zip"
-#   source   = "./out/lambda.zip"
-#   etag     = "${md5(file("./out/lambda.zip"))}"
-#   provider = "aws.home_region"
-# }
-
 module "us_east_1" {
-  source        = "./modules/lambda"
-  lambda_region = "us-east-1"
-
-  # s3_bucket     = "${aws_s3_bucket.artillery_packages.id}"
-  # s3_key        = "${aws_s3_bucket_object.artillery_package.id}"
-
+  source           = "./modules/lambda"
+  lambda_region    = "us-east-1"
   filename         = "./out/lambda.zip"
   function_name    = "artillery-us-east-1"
   role             = "${aws_iam_role.artillery.arn}"
@@ -33,12 +14,8 @@ module "us_east_1" {
 }
 
 module "us_east_2" {
-  source        = "./modules/lambda"
-  lambda_region = "us-east-2"
-
-  # s3_bucket     = "${aws_s3_bucket.artillery_packages.id}"
-  # s3_key        = "${aws_s3_bucket_object.artillery_package.id}"
-
+  source           = "./modules/lambda"
+  lambda_region    = "us-east-2"
   filename         = "./out/lambda.zip"
   function_name    = "artillery-us-east-2"
   role             = "${aws_iam_role.artillery.arn}"
@@ -48,12 +25,8 @@ module "us_east_2" {
 }
 
 module "us_west_1" {
-  source        = "./modules/lambda"
-  lambda_region = "us-west-1"
-
-  # s3_bucket     = "${aws_s3_bucket.artillery_packages.id}"
-  # s3_key        = "${aws_s3_bucket_object.artillery_package.id}"
-
+  source           = "./modules/lambda"
+  lambda_region    = "us-west-1"
   filename         = "./out/lambda.zip"
   function_name    = "artillery-us-west-1"
   role             = "${aws_iam_role.artillery.arn}"
