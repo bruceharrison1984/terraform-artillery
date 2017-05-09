de55bed1a72418ceb848adae4d6cdf5f96b389ea
Bruce Harrison <leeharrison1984@gmail.com>
2017-05-09 18:35:17 -0500

    Restructure project

--- index.js
+++ index.js
@@ -2,31 +2,33 @@
 'use strict';
 
 const program = require('commander');
-const terraform = require('./lib/terraform');
-const artillery = require('./lib/artillery.js');
+const terraformService = require('./lib/terraformService');
+const lambdaService = require('./lib/lambdaService');
 
 program.version('0.0.1');
 
 program
     .command('deploy')
     .description('deploy artillery lambdas to AWS environment')
-    .action(terraform.deploy);
+    .action(terraformService.deploy);
 
 program
     .command('package')
     .description('zip handler for deployment to AWS lambda')
-    .action(terraform.package);
+    .action(terraformService.package);
 
 program
     .command('destroy')
     .description('remove artillery lambdas from AWS environment')
-    .action(terraform.destroy);
+    .action(terraformService.destroy);
 
 program
     .command('invoke')
     .description('run scenario script against remote artillery lambdas')
     .option('-s, --script <script>', 'The scenario file to remotely execute on the artillery lambdas')
     .option('-i, --iterations <iterations>', 'The number of times to execute each scenario on each lambda')
-    .action(artillery.invoke);
+    .action(lambdaService.invoke);
 
 program.parse(process.argv);
+
+
--- terraform_templates/main.tf
+++ terraform_templates/main.tf
@@ -3,46 +3,46 @@ provider "aws" {
 }
 
 module "us_east_1" {
-  source           = "./modules/lambda"
+  source           = "./lambda"
   lambda_region    = "us-east-1"
-  filename         = "./out/lambda.zip"
+  filename         = "../lambda_package/lambda.zip"
   function_name    = "artillery-us-east-1"
   role             = "${aws_iam_role.artillery.arn}"
   handler          = "handler.handler"
-  source_code_hash = "${base64sha256(file("out/lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda_package/lambda.zip"))}"
   runtime          = "nodejs4.3"
 }
 
 module "us_east_2" {
-  source           = "./modules/lambda"
+  source           = "./lambda"
   lambda_region    = "us-east-2"
-  filename         = "./out/lambda.zip"
+  filename         = "../lambda_package/lambda.zip"
   function_name    = "artillery-us-east-2"
   role             = "${aws_iam_role.artillery.arn}"
   handler          = "handler.handler"
-  source_code_hash = "${base64sha256(file("out/lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda_package/lambda.zip"))}"
   runtime          = "nodejs4.3"
 }
 
 module "us_west_1" {
-  source           = "./modules/lambda"
+  source           = "./lambda"
   lambda_region    = "us-west-1"
-  filename         = "./out/lambda.zip"
+  filename         = "../lambda_package/lambda.zip"
   function_name    = "artillery-us-west-1"
   role             = "${aws_iam_role.artillery.arn}"
   handler          = "handler.handler"
-  source_code_hash = "${base64sha256(file("out/lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda_package/lambda.zip"))}"
   runtime          = "nodejs4.3"
 }
 
 module "us_west_2" {
-  source           = "./modules/lambda"
+  source           = "./lambda"
   lambda_region    = "us-west-2"
-  filename         = "./out/lambda.zip"
+  filename         = "../lambda_package/lambda.zip"
   function_name    = "artillery-us-west-2"
   role             = "${aws_iam_role.artillery.arn}"
   handler          = "handler.handler"
-  source_code_hash = "${base64sha256(file("out/lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda_package/lambda.zip"))}"
   runtime          = "nodejs4.3"
 }
 
