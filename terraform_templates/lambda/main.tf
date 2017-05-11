904d7397ab173f9fb403a2288509888c587f2702
Bruce Harrison <leeharrison1984@gmail.com>
2017-05-10 23:53:36 -0500

    Allow lambdas to log to cloudwatch

--- terraform_templates/lambda/main.tf
+++ terraform_templates/lambda/main.tf
@@ -2,7 +2,8 @@ variable "enabled" {}
 variable "lambda_region" {}
 variable "filename" {}
 variable "function_name" {}
-variable "role" {}
+variable "role_arn" {}
+variable "role_id" {}
 variable "handler" {}
 variable "source_code_hash" {}
 variable "runtime" {}
@@ -21,7 +22,7 @@ resource "aws_lambda_function" "artillery_lambda" {
 
   count            = "${var.enabled}"
   filename         = "${var.filename}"
-  role             = "${var.role}"
+  role             = "${var.role_arn}"
   handler          = "${var.handler}"
   source_code_hash = "${var.source_code_hash}"
   runtime          = "${var.runtime}"
@@ -32,4 +33,32 @@ resource "aws_lambda_function" "artillery_lambda" {
   environment {
     variables = "${var.env_vars}"
   }
+}
+
+resource "aws_iam_role_policy" "artillery_cloudwatch_policy" {
+  name  = "${var.function_name}-cloudwatch-policy"
+  role  = "${var.role_id}"
+  count = "${var.enabled}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_cloudwatch_log_group.artillery_logs.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "artillery_logs" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 90
+  count             = "${var.enabled}"
 }
