a931836d0c963583aed0fbc0f38a677f26f5d645
Bruce Harrison <leeharrison1984@gmail.com>
2017-05-24 17:28:58 -0500

    Move index.js into bin folder so paths can be resolved within node_modules without using __dirname

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
-const packageInfo = require(`${__dirname}/package.json`);
+const packageInfo = require('../package.json');
 const program = require('commander');
-const terraformService = require(`${__dirname}/lib/terraformService`);
-const artilleryService = require(`${__dirname}/lib/artilleryService`);
+const terraformService = require('../lib/terraformService');
+const artilleryService = require('../lib/artilleryService');
 
 program.version(packageInfo.version);
 
