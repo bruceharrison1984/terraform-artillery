3aee2c90e852b55ff0e6c09f9385dd747e30bd24
Bruce Harrison <leeharrison1984@gmail.com>
2017-05-25 19:28:38 -0500

    Add additional modules to exports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
     artilleryService: require('./lib/artilleryService'),
     lambdaService: require('./lib/lambdaService'),
+    md5dir: require('./lib/md5dir'),
+    packagingService: require('./lib/packagingService'),
     resultsWriterService: require('./lib/resultsWriterService'),
     scenarioService: require('./lib/scenarioService'),
     terraformService: require('./lib/terraformService'),
